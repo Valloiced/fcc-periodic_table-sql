a7c73f95272987a1795a02126d7f2a47e08a0979
Valloiced <vinhs1605@gmail.com>
2024-01-27 21:55:50 -0500

    refactor: added echo functions to avoid errors during testings

--- element.sh
+++ element.sh
@@ -4,25 +4,27 @@ echo 'Please provide an element as an argument'
 
 # Check input if atomic number, symbol or element name
 CHECK_INPUT() {
-
+  echo "check input"
 }
 
 # Check element if retrievable using atomic number
 GET_ELEMENT_ATOMIC_NUMBER() {
-
+  echo "get element by number"
 }
 
-# Check element if retrievable using atomic number
+# Check element if retrievable using atomic symbol
 GET_ELEMENT_SYMBOL() {
+  echo "get element by symbol"
 }
 
-# Check element if retrievable using atomic number
+# Check element if retrievable using atomic name
 GET_ELEMENT_NAME() {
+  echo "get element by name"
 }
 
-# Check element if retrievable using atomic number
+# If not found
 NOT_FOUND() {
-
+  echo "not found"
 }
 
 # START
