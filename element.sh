de66bf49a501c567e986e4ff8c507c852c775b01
Valloiced <vinhs1605@gmail.com>
2024-01-27 21:47:59 -0500

    Initial commit

---
+++ element.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+echo 'Please provide an element as an argument'
+
+# Check input if atomic number, symbol or element name
+CHECK_INPUT() {
+
+}
+
+# Check element if retrievable using atomic number
+GET_ELEMENT_ATOMIC_NUMBER() {
+
+}
+
+# Check element if retrievable using atomic number
+GET_ELEMENT_SYMBOL() {
+}
+
+# Check element if retrievable using atomic number
+GET_ELEMENT_NAME() {
+}
+
+# Check element if retrievable using atomic number
+NOT_FOUND() {
+
+}
+
+# START
+CHECK_INPUT
